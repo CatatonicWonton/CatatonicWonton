6e7ab7ac69959c7e7a856e4ec4b443446099ad94
jplourd <jeffplourd@gmail.com>
2015-08-29 16:42:03 -0700

    Check in login progress.

--- client/app/login/loginSpec.js
+++ client/app/login/loginSpec.js
@@ -1,3 +1,17 @@
 describe('loginCtrl', function(){
+  beforeEach(module('app'));
 
+  var $controller;
+
+  beforeEach(inject(function (_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('loginCtrl', function(){
+    it('is defined', function(){
+      $scope = {};
+      var controller = $controller('loginCtrl', { $scope : $scope});
+      expect(controller).toBeDefined();
+    })
+  });
 });
