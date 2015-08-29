c12c354250bb737e7876d97cfaed5aca8fe2d87c
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-29 11:49:51 -0700

    add documentation for data model schema

---
+++ server/APIdocs.js
@@ -0,0 +1,105 @@
+/* 
+  GET /projects 
+  
+  {
+    projects: [
+      {
+        name: String,
+        subject: String,
+        author: String,
+        pages: [
+          {
+            title: String,
+            content: String,
+            answer: String
+          }
+        ],  
+      }
+    ]
+  }
+ */
+
+/* 
+  GET /projects/:id
+
+  {
+    name: String,
+    subject: String,
+    author: String,
+    pages: [
+      {
+        title: String,
+        content: String,
+        answer: String
+      }
+    ],
+  }
+ */
+
+ /* 
+  POST /projects
+
+  {
+    name: String,
+    subject: String,
+    author: String,
+    pages: [
+      {
+        title: String,
+        content: String,
+        answer: String
+      },
+    ],
+
+  }
+ */
+
+/* 
+  GET /class/:id
+  
+  {
+    students: [
+      {
+        id: Number,
+        name: String,
+        {
+          id: Id,
+          firstName: String,
+          lastName: String,
+          subject: String,
+          author: String,
+          pages: [
+            {
+              title: String,
+              content: String,
+              answer: String
+            },
+          ],
+        }
+        currentPage: {
+          title: String,
+          timePageStarted: Time
+        },
+
+      },
+    ]
+  }
+*/
+
+/* 
+  POST /class/:id
+
+  {
+    studentId: Id
+  }
+ */
+
+/* 
+  POST /students
+  
+  {
+    firstName: String,
+    lastName: String
+  }
+ */
+
