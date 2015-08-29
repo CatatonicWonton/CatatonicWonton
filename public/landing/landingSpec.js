d77dd929b9832cbd184e43d81c1a18103085bad2
jplourd <jeffplourd@gmail.com>
2015-08-29 12:08:24 -0700

    Adds dependencies in package.json and test structure to landing.js

---
+++ public/landing/landingSpec.js
@@ -0,0 +1,14 @@
+describe('landingCtrl', function() {
+
+  beforeEach(module('landingCtrl'));
+
+  describe('landingCtrl controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var landingCtrl = $controller('landingCtrl');
+      expect(landingCtrl).toBeDefined();
+    }));
+
+  });
+});
