0165b54a322ae278db766a5c0485141e2e439625
jplourd <jeffplourd@gmail.com>
2015-09-01 09:51:06 -0700

    Creates basic outline for teacherHome

--- client/app/teacherHome/teacherHomeCtrl.js
+++ client/app/teacherHome/teacherHomeCtrl.js
@@ -1,7 +1,18 @@
 angular.module('app')
-  .controller('teacherHomeCtrl', function teacherHomeCtrl($scope){
-    $scope.teacherProjects = [];
+  .controller('teacherHomeCtrl', function teacherHomeCtrl($scope, projectFactory){
+    
+    // this is the array of current project for this teacher
+    $scope.teacherProjects = projectFactory.projects;
+    
     $scope.addProject = function() {
-      console.log('hey');
+      console.log('add new project:', projectFactory.projects);
+      // launch modal that gathers information about new project
+        // add new information to current teacher's project data
+      // transition to 'teacherProject' view with new information
     };
+    $scope.editProject = function(index) {
+      console.log('Edit this project:', $scope.teacherProjects[index]);
+      // transition state to the selected projects edit view
+    }
+
   });
