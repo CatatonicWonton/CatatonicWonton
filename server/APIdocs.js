16fe2ffedfa4cebb69f70ba893b9b2d4536d30d4
jplourd <jeffplourd@gmail.com>
2015-09-02 10:57:55 -0700

    updates server API docs

--- server/APIdocs.js
+++ server/APIdocs.js
@@ -22,18 +22,81 @@
 /* 
   GET /projects/:id
 
-  {
-    name: String,
-    subject: String,
-    author: String,
-    pages: [
-      {
-        title: String,
-        content: String,
-        answer: String
-      }
-    ],
-  }
+  var Projects = [
+    {
+      projectId: '1',
+      title: 'BugProject',
+      subject: 'Science',
+      author: 'Adnan',
+      pages: [
+        {
+          pageId: '1',
+          title: 'BugLife',
+          content: [
+            {
+              contentId: '1',
+              actualContent: '<p>Bugs are strange creatures. What are your favorite bugs?</p>'
+            },
+            {
+              contentId: '2',
+              actualContent: '<div>something else here'
+            }
+          ]
+        },
+        {
+          pageId: '2',
+          title: '',
+          content: [
+            {
+              contentId: '1',
+              actualContent: '<p>Bugs are strange creatures. What are your favorite bugs?</p>'
+            },
+            {
+              contentId: '2',
+              actualContent: '<div>something else here'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      projectId: '2',
+      title: '',
+      subject: '',
+      author: 'Jeffrey'
+      pages: [
+        {
+          pageId: '1',
+          title: '',
+          content: [
+            {
+              contentId: '1',
+              actualContent: '<p>Bugs are strange creatures. What are your favorite bugs?</p>'
+            },
+            {
+              contentId: '2',
+              actualContent: '<div>something else here'
+            }
+          ]
+        },
+        {
+          pageId: '2',
+          title: '',
+          content: [
+            {
+              contentId: '1',
+              actualContent: '<p>Bugs are strange creatures. What are your favorite bugs?</p>'
+            },
+            {
+              contentId: '2',
+              actualContent: '<div>something else here'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
  */
 
  /* 
