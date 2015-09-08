a6a38bd859dac9f147864336d59677dbbcc757a0
Devon Koch <devon.r.koch@gmail.com>
2015-09-08 15:55:48 -0700

    creatingAccount capability, added utility function 'findUsername'

--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@ var express = require('express');
 var db = require('./db.js');
 var sequelize = require('sequelize');
 var Promise = require('bluebird');
+var Models = db.Models;
+var Utils = require('./utilities.js');
 
 // Auth
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-// Auth models
-var StudentModel = db.Models.Student;
-var TeacherModel = db.Models.Teacher;
-
 
 // middleware
 var cookieParser = require('cookie-parser');
@@ -28,7 +26,8 @@ var app = express();
 
 // app.use(bodyParser.urlencoded({extended: true}));
 
-// passport middleware
+app.use(express.static(__dirname + '/../client'));
+// middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(session({ secret: 'SECRET' }));
@@ -36,54 +35,72 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Authentication
-
 // TODO: sign up
 
-
 // sign in
-Promise
-  .some([
-    StudentModel.findOne({
-      where: {username: username}
-    }),
-    TeacherModel.findOne({
-      where: {username: username}
-    })
-  ], 1)
-  .then(function (user) {
-    if(user.password === password) {
-      return done(null, user);
-    }
-    return done(null, false, { message: 'Incorrect password.' });
-  })
-  .catch(AggregateError, function(err) {
-    return done(null, false, { message: 'Incorrect username.' });
-  });
-
 passport.use(new LocalStrategy(
   function(username, password, done) {
     Promise
       .some([
-        StudentModel.findOne({
+        Models.Student.findOne({
           where: {username: username}
         }),
-        TeacherModel.findOne({
+        Models.Teacher.findOne({
           where: {username: username}
         })
       ], 1)
-      .then(function (user) {
-        if(user.password === password) { // ???
+      .spread(function (user) {
+        if(!user) {
+          return done(null, false, { message: 'Incorrect username or password.' });
+        }
+        if(user.password === password) { 
           return done(null, user);
         }
-        return done(null, false, { message: 'Incorrect password.' });
       })
-      .catch(AggregateError, function(err) {
-        return done(null, false, { message: 'Incorrect username.' });
+      .catch(function(err) { // catches in the event of database failure
+        return done(err);
       });
   }
 ));
 
-app.use(express.static(__dirname + '/../client'));
+// session support
+passport.serializeUser(function(user, done) {
+  done(null, user.username); // we use ".username" instead of ".id" becuase teacher and student ids can collide
+});
+
+passport.deserializeUser(function(username, done) {
+  Promise
+    .some([
+      Models.Student.findOne({
+        where: {username: username}
+      }),
+      Models.Teacher.findOne({
+        where: {username: username}
+      })
+    ], 1)
+    .spread(function (user) {
+      return done(null, user);
+    })
+    .catch(function(err) {
+      return done(err);
+    });
+});
+
+function createAccount (req, res) {
+  var accountType = req.body.accountType;
+  var username = req.body.username;
+
+  Utils
+    .findUsername(username)
+    .spread(function (user) {
+      if (!user) {
+        Models[accountType]
+          .create(req.body);
+      } else {
+        res.send('Username is already taken!');
+      }
+    })
+};
 
 // Define Routes
 app.use('/api/class', classRoutes);
