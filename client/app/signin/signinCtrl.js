afee18b6ba46855f8633ea1ff29fef6043156eab
Devon Koch <devon.r.koch@gmail.com>
2015-09-09 16:23:17 -0700

    fornt end auth

--- client/app/signin/signinCtrl.js
+++ client/app/signin/signinCtrl.js
@@ -2,8 +2,8 @@ angular.module('app')
   .controller('signinCtrl', function($scope, $state, User){
     // create function to handle the request to our api to signin and signup
     $scope.signin = function(username, password) {
-      User.signin(username, password).then(function(type){
-        type === 'teacher' ? $state.go('teacherHome') : $state.go('studentHome');
+      User.signin(username, password).then(function(){
+        // type === 'teacher' ? $state.go('teacherHome') : $state.go('studentHome');
       });
     };
   });
