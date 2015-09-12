8d1ad7668f9234d11f624a1311a393b14c230270
Devon Koch <devon.r.koch@gmail.com>
2015-09-12 12:56:11 -0700

    passwords must match on signup, else error

--- client/app/signup/signupCtrl.js
+++ client/app/signup/signupCtrl.js
@@ -2,25 +2,30 @@ angular.module('app')
   .controller('signupCtrl', function($scope, $state, User){
     // create function to handle the request to our api to signin and signup
     $scope.hideAlreadyRegistered = true;
+    $scope.hidePasswordMatch = true;
 
-    $scope.signup = function(firstName, lastName, username, password, accountType) {
-      User.signup(firstName, lastName, username, password, accountType).then(function(res){
-        
-        if(res.data === 'Username is already taken!') {
-          $scope.hideAlreadyRegistered = false;
-        }
+    $scope.signup = function(firstName, lastName, username, password, confirmedPassword, accountType) {
+      if(password !== confirmedPassword) {
+        $scope.hidePasswordMatch = false;
+      } else {
+        $scope.hidePasswordMatch = true;
+        User.signup(firstName, lastName, username, password, accountType).then(function(res){
+          if(res.data === 'Username is already taken!') {
+            $scope.hideAlreadyRegistered = false;
+          }
 
-        if(res.data === true) {
-          User.signin(username, password).then(function(response){
-            if (response.data.accountType === 'Teacher') {
-              $state.go('teacherHome');
-            } else {
-              $state.go('studentHome');
-            }
-          });
-        }
-
-      });
+          if(res.data === true) {
+            User.signin(username, password).then(function(response){
+              if (response.data.accountType === 'Teacher') {
+                $state.go('teacherHome');
+              } else {
+                $state.go('studentHome');
+              }
+            });
+          }
+        });
+      }
+      
     };
 
 
