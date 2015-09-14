a9b5831a664dfc6cc04fc7accea8302bcaab06da
jplourd <jeffplourd@gmail.com>
2015-09-14 10:56:25 -0700

    hides signin/signput in navbar

--- client/app/navbar/navbarCtrl.js
+++ client/app/navbar/navbarCtrl.js
@@ -1,17 +1,20 @@
 angular.module('app')
   .controller('navbarCtrl', function($scope, $state, $http, $rootScope, User) {
+    
+    $scope.navBarCollapsed = true;    
+    $scope.loggedIn = false;
 
     $scope.logout = function() {
       $http.post('/auth/logout', {}).then(function(response) {
+        $scope.loggedIn = false;
         $state.go('signin');
       });
     };
 
-    $scope.navBarCollapsed = true;    
-
     // makes sure the nav bar only shows the appropriate links for the user
     var setNavbar = function() {
       var accountType = User.getUser().accountType;
+      $scope.loggedIn = true;
       $scope.isTeacher = accountType === 'Teacher' ? true : false;
     };
 
