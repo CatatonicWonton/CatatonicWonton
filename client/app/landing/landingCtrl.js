a0120f1d0321aeece93cef21a0815d81ae1973e8
jplourd <jeffplourd@gmail.com>
2015-09-18 15:43:42 -0700

    Adds landing functionality

--- client/app/landing/landingCtrl.js
+++ client/app/landing/landingCtrl.js
@@ -2,31 +2,39 @@ angular.module('app')
   .controller('landingCtrl', function ($scope){
     $scope.test = 'test';
     $scope.options = {
-    chart: {
-      type: 'pieChart',
-      height: 450,
-      donut: true,
-      x: function(d){return d.key;},
-      y: function(d){return d.y;},
-      showLabels: true,
-      pie: {
-        startAngle: function(d) { return d.startAngle -Math.PI },
-        endAngle: function(d) { return d.endAngle -Math.PI }
-      },
-      transitionDuration: 500,
-      style: {
-        'color': 'red',
-        'background-color': 'blue'
-      },
-      legend: {
-        margin: {
-          top: 5,
-          right: 140,
-          bottom: 5,
-          left: 0
+      chart: {
+        type: 'pieChart',
+        height: 450,
+        donut: true,
+        x: function(d){return d.key;},
+        y: function(d){return d.y;},
+        showLabels: true,
+        pie: {
+          startAngle: function(d) { return d.startAngle -Math.PI },
+          endAngle: function(d) { return d.endAngle -Math.PI }
+        },
+        color: [
+          '#ededf0',
+          '#79a9ab',
+          '#192e30',
+          '#bdd9d8',
+          '#3a585c',
+          '#3f5e4a'
+        ],
+        transitionDuration: 500,
+        style: {
+          'color': 'red',
+          'background-color': 'blue'
+        },
+        legend: {
+          margin: {
+            top: 5,
+            right: 140,
+            bottom: 5,
+            left: 0
+          }
         }
       }
-    }
     };
 
      $scope.data = [
