0a5d3c369c935b79afb767aebd54076ff0273037
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-09-22 15:04:21 -0700

    use postgres database for heroku

--- server/db.js
+++ server/db.js
@@ -1,8 +1,24 @@
 var config = require('./config/config');
 var Promise = require('bluebird');
 var Sequelize = require('sequelize');
-var DATABASE_URL = process.env.DATABASE_URL || 'schoolio';
-var sequelize = new Sequelize(DATABASE_URL, 'root', '', {logging: false});  
+var sequelize;
+
+if (process.env.DATABASE_URL) {
+  var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging: false,
+      dialectOptions: {
+          ssl: true
+      }
+  });
+} else {
+  sequelize = new Sequelize('schoolio', 'root', '', {logging: false});
+}
+
 
 // MODELS
 var Models         = {};
