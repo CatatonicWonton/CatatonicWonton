b1c7cf87c9ff4acf231d9688314270d793d29bc4
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-09-27 20:21:26 -0700

    added persistNav function from user.js factory

--- client/app/navbar/navbarCtrl.js
+++ client/app/navbar/navbarCtrl.js
@@ -10,18 +10,23 @@ angular.module('app')
       User.logout($scope);
     };
 
+
     // makes sure the nav bar only shows the appropriate links for the user
     $scope.setNavbar = function() {
-      User.getUser().then(function (object) {
-        var accountType = object.data.accountType;
-        // console.log('navbarCtrl.js: accountType: ',accountType);
+
+      var accountType = User.getUserObj().accountType;
+
+      // User.getUser().then(function (object) {
+      //   var accountType = object.data.accountType;
+      //   // console.log('navbarCtrl.js: accountType: ',accountType);
         $scope.loggedIn = accountType === undefined ? false : true;
         $scope.isTeacher = accountType === 'Teacher' ? true : false;
-        // console.log('navbarCtrl.js: isTeacher: ', $scope.isTeacher);
-      });
+      //   // console.log('navbarCtrl.js: isTeacher: ', $scope.isTeacher);
+      // });
     };
 
-    $scope.setNavbar();
+
+    User.persistNav().then($scope.setNavbar);
 
     $rootScope.$on('$stateChangeStart', $scope.setNavbar);
     $rootScope.$on('teacherOrStudent', $scope.setNavbar);
